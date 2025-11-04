71c81af49b2f4525e238b49cda6f3548c7dc7298
Claire Economopoulou <klairh15@gmail.com>
2025-11-04 10:32:48 +0000

    Initial commit

--- mock_api.py
+++ mock_api.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Allow Streamlit frontend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # for local dev; restrict later
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
